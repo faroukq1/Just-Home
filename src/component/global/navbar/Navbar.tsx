c6e32516378e588979578b88cfe2d4dabf3f8cbd
faroukq1 <faroukq1@gmail.com>
2024-07-08 19:34:42 +0100

    #8 navbar folder

--- src/component/global/navbar/Navbar.tsx
+++ src/component/global/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import logo from "../../../assets/logo.png";
 import Navlinks from "./Navlinks";
 
 const Navbar = () => {
--- src/component/index.ts
+++ src/component/index.ts
@@ -1,7 +1,7 @@
 export {default as Erorr} from './global/Error';
 export {default as LoadingSpinner} from './global/LoadingSpinner';
-export {default as Navbar} from './global/Navbar';
-export {default as Navlinks} from './global/Navlinks';
+export {default as Navbar} from './global/navbar/Navbar';
+export {default as Navlinks} from './global/navbar/Navlinks';
 export {default as RecomendedHomes} from './landing/RecomendedHomes';
 export {default as Hero} from './landing/Hero';
 export {default as PropertyCard} from './global/PropertyCard';
