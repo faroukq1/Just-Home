24d4037fde866024b9ab9491af4b23b0e2579047
faroukq1 <faroukq1@gmail.com>
2024-07-12 15:45:36 +0100

    #23 fix css bug buy page

--- src/pages/BuyProperty.tsx
+++ src/pages/BuyProperty.tsx
@@ -2,9 +2,9 @@ import { BuyFilter, BuySearchBar, PropertyMarketPlace } from "../component";
 
 const BuyProperty = () => {
   return (
-    <div className="pt-20 h-screen mx-auto px-8 max-w-screen-2xl">
+    <div className="pt-20 min-h-screen mx-auto px-8 max-w-screen-2xl">
       <BuySearchBar />
-      <div className="grid grid-cols-auto-1fr gap-4">
+      <div className="flex gap-4">
         <BuyFilter />
         <PropertyMarketPlace />
       </div>
