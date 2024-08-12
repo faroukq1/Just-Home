b547bced19c40e0503660e571761c7bd7a44686a
faroukq1 <faroukq1@gmail.com>
2024-08-12 14:50:19 +0100

    fix property features

--- src/component/products/PropertyDescription.tsx
+++ src/component/products/PropertyDescription.tsx
@@ -8,6 +8,12 @@ import { useLoaderData } from "react-router-dom";
 const PropertyDescription = () => {
   const { data }: any = useLoaderData();
   const { title, description, address } = data.attributes;
+  const {
+    wifi,
+    beds,
+    bathrooms,
+    dimension: { width, height },
+  } = data.attributes.features.features;
   return (
     <div>
       <div className="my-6 flex flex-col gap-4">
@@ -21,20 +27,20 @@ const PropertyDescription = () => {
       <div className="flex flex-col gap-6">
         <h3 className="text-xl font-bold tracking-wide">Hotel features</h3>
         <div className="flex justify-between">
+          {wifi ? (
+            <span className="flex items-center gap-2 text-md text-neutral-500">
+              <FaWifi className="text-2xl" /> Wifi
+            </span>
+          ) : null}
           <span className="flex items-center gap-2 text-md text-neutral-500">
-            <FaWifi className="text-2xl" /> Wifi
+            <LuBedDouble className="text-2xl" /> {beds} Beds
           </span>
           <span className="flex items-center gap-2 text-md text-neutral-500">
-            <LuBedDouble className="text-2xl" /> Beds
+            <FaBath className="text-2xl" /> {bathrooms} Bathroom
+            {bathrooms > 1 && "s"}
           </span>
           <span className="flex items-center gap-2 text-md text-neutral-500">
-            <FaBath className="text-2xl" /> Bathroom
-          </span>
-          <span className="flex items-center gap-2 text-md text-neutral-500">
-            <MdOutlineBreakfastDining className="text-2xl" /> Break fast
-          </span>
-          <span className="flex items-center gap-2 text-md text-neutral-500">
-            <BsArrowsFullscreen className="text-2xl" /> 4m x 6m
+            <BsArrowsFullscreen className="text-2xl" /> {width}m x {height}m
           </span>
         </div>
       </div>
