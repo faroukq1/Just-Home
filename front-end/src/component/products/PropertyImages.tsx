3f9c7b29c77ba478ea9b48a59fc392c75c5e14e8
faroukq1 <faroukq1@gmail.com>
2024-08-16 11:59:29 +0100

    fix property images

--- front-end/src/component/products/PropertyImages.tsx
+++ front-end/src/component/products/PropertyImages.tsx
@@ -10,25 +10,25 @@ const PropertyImages = () => {
     data.attributes.images.data[3 % length].attributes.formats.small.url;
   return (
     <div className="flex gap-4 h-[46%]">
-      <div>
+      <div className="flex-1 h-full">
         <img
           src={`http://localhost:1337${data.attributes.image.data.attributes.formats.medium.url}`}
           alt="hotel2"
           className="object-cover w-full h-full rounded-l-[1rem]"
         />
       </div>
-      <div style={{ flexGrow: "0.15" }} className="flex flex-col gap-2 h-full">
+      <div style={{ flexGrow: "0.1" }} className="flex flex-col gap-2 h-full">
         <img
           src={`http://localhost:1337${image1}`}
           alt="hotel2"
-          className="object-cover w-full h-1/3 rounded-tr-[1rem]"
+          className="object-cover w-full h-[32%] rounded-tr-[1rem]"
         />
         <img
           src={`http://localhost:1337${image2}`}
           alt="hotel2"
-          className="object-cover  h-1/3"
+          className="object-cover h-[32%]"
         />
-        <div className="w-full h-1/3 relative">
+        <div className="w-full h-[32%] relative">
           <img
             src={`http://localhost:1337${image3}`}
             alt="hotel4"
