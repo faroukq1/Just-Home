20729105012957f28daf11ea63ad783fee895b99
faroukq1 <faroukq1@gmail.com>
2024-08-16 11:50:00 +0100

    best property fix buttom and properties quantity

--- front-end/src/component/landing/BestProperty.tsx
+++ front-end/src/component/landing/BestProperty.tsx
@@ -21,15 +21,20 @@ const BestProperty = () => {
           <img src={decore} alt="decore" className="w-full h-[50%]" />
           <div className="flex flex-col md:flex-row gap-4 rounded-lg flex-1">
             <div className="relative bg-black p-10 flex-1 rounded-lg">
-              <h1 className="text-white text-4xl font-bold">280+</h1>
+              <h1 className="text-white text-4xl font-bold">
+                {data.length - 1}+
+              </h1>
               <p className="text-white font-semibold">property</p>
               <p className="text-sm mt-4 text-neutral-400">
                 Explore our wide variety of properties to fid your dream home
                 today.
               </p>
-              <div className="bg-white rounded-full w-fit p-3 absolute bottom-6 right-6">
+              <Link
+                to="/properties"
+                className="bg-white rounded-full w-fit p-3 absolute bottom-4 right-6"
+              >
                 <FaArrowRight className="text-xl" />
-              </div>
+              </Link>
             </div>
             <div className="flex-1">
               <img src={decore2} alt="decore2" className="h-full" />
