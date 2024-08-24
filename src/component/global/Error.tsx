962464094f1958bd5c58ab40a7651ea57c1c5203
faroukq1 <faroukq1@gmail.com>
2024-08-24 12:32:55 +0100

    redesign error page

--- src/component/global/Error.tsx
+++ src/component/global/Error.tsx
@@ -1,13 +1,21 @@
-import { useRouteError } from "react-router-dom";
-
+import { Link, useRouteError } from "react-router-dom";
+import errorImage from "../../assets/error.png";
 const Error = () => {
   const error = useRouteError();
   console.log(error);
   return (
-    <div className="h-screen flex items-center justify-center bg-slate-500">
-      <h1 className="text-white font-bold">
-        Oops , sorry there is something happen...
-      </h1>
+    <div className="h-screen flex flex-col items-center bg-neutral">
+      <div className="mt-40 mb-10 grid gap-8">
+        <img src={errorImage} alt="error image" className="object-contain" />
+        <h1 className="text-center text-white font-bold text-2xl">
+          Oops , sorry there is something happen...
+        </h1>
+        <div className="grid place-items-center">
+          <Link to="/" className="btn btn-wide">
+            Go Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
