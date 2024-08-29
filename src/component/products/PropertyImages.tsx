96eac1f1b8cbf7abd5f39cd7b79ddbc239d06b54
faroukq1 <faroukq1@gmail.com>
2024-08-29 19:02:40 +0100

    add arrow to carrosel property info

--- src/component/products/PropertyImages.tsx
+++ src/component/products/PropertyImages.tsx
@@ -1,11 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import ImageWithBlurHash from "../global/ImageWithBlurHash";
+import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
 
 const PropertyImages = () => {
   const { data }: any = useLoaderData();
   const propertyAllImages = data.fields.images;
   return (
-    <div className="carousel carousel-center bg-neutral rounded-box space-x-4 p-4">
+    <div className="relative carousel carousel-center bg-neutral rounded-box space-x-4 p-4">
+      <button className="absolute left-0 my-auto h-full z-[10]">
+        <CiCircleChevLeft className="text-5xl text-black" />
+      </button>
       {propertyAllImages.map((item: any) => {
         return (
           <div className="carousel-item" key={item.sys.id}>
@@ -13,18 +17,15 @@ const PropertyImages = () => {
               blurhash="LEHV6nWB2yk8pyo0adR*.7kCMdnj"
               imageUrl={`https:${item.fields.file.url}`}
               alt="image"
-              width={400}
-              height={400}
+              width={800}
+              height={800}
             />
-            {/* <img
-              src={`https:${item.fields.file.url}`}
-              alt="aa"
-              className="rounded-box w-96 h-96"
-              loading="lazy"
-            /> */}
           </div>
         );
       })}
+      <button className="absolute right-0 z-[10] my-auto h-full">
+        <CiCircleChevRight className="text-5xl text-black" />
+      </button>
     </div>
   );
 };
