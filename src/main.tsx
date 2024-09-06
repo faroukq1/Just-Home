cd4209cabe59b4dfb601e0ca79ac0b1b94e79d8c
faroukq1 <faroukq1@gmail.com>
2024-09-06 17:05:30 +0100

    add auth0 to the projet

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    onRedirectCallback={(appState) => {
+      window.history.replaceState(
+        {},
+        document.title,
+        appState?.returnTo || window.location.pathname
+      );
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
